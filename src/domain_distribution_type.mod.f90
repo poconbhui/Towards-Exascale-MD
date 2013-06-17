0f01258eb7ba736c584f9d10d859d3bc3b1520e7
Padraig O Conbhui <poconbhui@gmail.com>
2013-06-17 05:14:59 +0100

    Added particle redistribution in domain distribution.

--- src/domain_distribution_type.mod.f90
+++ src/domain_distribution_type.mod.f90
@@ -14,6 +14,7 @@ module domain_distribution_type
         integer, private :: num_local_particles
         real(p), private :: domain_size(Ndim)
 
+        integer, private, allocatable :: particle_ids(:)
         type(particle), private, allocatable :: particles(:)
 
         integer, private :: rank
@@ -29,54 +30,177 @@ module domain_distribution_type
         procedure :: print_string
 
         procedure, private :: get_foreign_list
+        procedure :: balance_lists
     end type domain_distribution
     interface domain_distribution
         module procedure constructor
     end interface domain_distribution
 
 contains
-    function constructor(num_particles, domain_size, comm)
+    function constructor(num_particles, domain_size, initial_distribution, comm)
         type(domain_distribution) :: constructor
         integer, intent(in) :: num_particles
         real(p), intent(in) :: domain_size(Ndim)
+        procedure(one_particle_function) :: initial_distribution
         integer, intent(in) :: comm
 
 
-        call constructor%init(num_particles, domain_size, comm)
+        call constructor%init( &
+            num_particles, domain_size, initial_distribution, &
+            comm &
+        )
     end function constructor
 
-    subroutine init(this, num_particles, domain_size, comm)
+    subroutine init( &
+        this, &
+        num_particles, domain_size, initial_distribution, &
+        comm &
+    )
         class(domain_distribution), intent(out) :: this
         integer, intent(in) :: num_particles
         real(p), intent(in) :: domain_size(Ndim)
+        procedure(one_particle_function) :: initial_distribution
         integer, intent(in) :: comm
 
+        type(particle) :: tmp_particle
         integer :: chunk_size
+        real(p) :: domain_start
+        real(p) :: domain_end
+
         integer :: comm_dup
+
+        integer :: i
 
         integer :: ierror
 
 
+        !
+        ! Set up MPI stuff
+        !
         call MPI_Comm_dup(comm, comm_dup, ierror)
+        call MPI_Comm_rank(comm_dup, this%rank, ierror)
+        call MPI_Comm_size(comm_dup, this%nprocs, ierror)
 
+        ! Set some distribution data
         this%num_particles = num_particles
         this%domain_size   = domain_size
         this%comm          = comm_dup
 
-
-        call MPI_Comm_rank(comm_dup, this%rank, ierror)
-        call MPI_Comm_size(comm_dup, this%nprocs, ierror)
-
+        !
         ! Find initial particle distribution size
-        chunk_size = ceiling(real(this%num_particles)/this%nprocs)
-        this%num_local_particles = chunk_size
-        if((this%rank+1)*chunk_size .GT. num_particles) then
-            this%num_local_particles = this%num_local_particles &
-                - ((this%rank+1)*chunk_size - num_particles)
-        end if
+        ! and initialize particles
+        !
+!        chunk_size = ceiling(real(this%num_particles)/this%nprocs)
+!        this%num_local_particles = chunk_size
+!        if((this%rank+1)*chunk_size .GT. num_particles) then
+!            this%num_local_particles = this%num_local_particles &
+!                - ((this%rank+1)*chunk_size - num_particles)
+!        end if
+        
+        ! Domain decomposed over x direction
+        chunk_size = 0
+        domain_start = this%rank*domain_size(1)/this%nprocs
+        domain_end   = (this%rank+1)*domain_size(1)/this%nprocs
+        if(this%rank .EQ. this%nprocs-1) domain_end = domain_end + 1
 
-        allocate(this%particles(this%num_local_particles))
+        do i=1, num_particles
+            tmp_particle = particle(pos=0, vel=0, force=0, mass=1)
+            tmp_particle = initial_distribution(tmp_particle, i)
+
+            if( get_rank_by_particle(this, tmp_particle) .EQ. this%rank) then
+                chunk_size = chunk_size + 1
+
+                call resize_particles(this, chunk_size)
+
+                this%particle_ids(chunk_size) = i
+                this%particles(chunk_size) = tmp_particle
+
+                this%num_local_particles = chunk_size
+            end if
+        end do
+
     end subroutine init
+
+    ! Define decomposition pattern
+    function get_rank_by_particle(this, pi)
+        integer :: get_rank_by_particle
+
+        class(domain_distribution), intent(inout) :: this
+        type(particle), intent(in) :: pi
+
+
+        ! Distribute particles by x coordinate
+        get_rank_by_particle = int( &
+            abs(pi%pos(1))/(this%domain_size(1)/this%nprocs) &
+        )
+
+        if(get_rank_by_particle .GT. this%nprocs-1) then
+            get_rank_by_particle = this%nprocs-1
+        end if
+    end function get_rank_by_particle
+
+    subroutine resize_particles(this, new_size_in)
+        class(domain_distribution), intent(inout) :: this
+        integer, intent(in) :: new_size_in
+
+        integer :: new_size
+
+        integer, allocatable :: old_particle_ids(:)
+        type(particle), allocatable :: old_particles(:)
+
+
+        new_size = new_size_in
+
+
+        ! If new size is only slightly bigger, double current allocation
+        ! to avoid potentially more allocations.
+        !
+        ! If new size is only slightly smaller, do nothing.
+
+        if(.NOT. allocated(this%particles)) then
+            allocate(this%particle_ids(new_size))
+            allocate(this%particles(new_size))
+        else if(new_size .GT. size(this%particles)) then
+            allocate(old_particle_ids(size(this%particle_ids)))
+            allocate(old_particles(size(this%particles)))
+
+            old_particle_ids = this%particle_ids
+            old_particles = this%particles
+
+            deallocate(this%particle_ids)
+            deallocate(this%particles)
+
+            if(new_size .LT. 2*size(this%particles)) then
+                new_size = 2*size(this%particles)
+            end if
+
+            allocate(this%particle_ids(new_size))
+            allocate(this%particles(new_size))
+
+            this%particle_ids(1:size(old_particles)) = old_particle_ids
+            this%particles(1:size(old_particles)) = old_particles
+
+            deallocate(old_particle_ids)
+            deallocate(old_particles)
+        else if(new_size .LT. 0.5*size(this%particles)) then
+            allocate(old_particle_ids(new_size))
+            allocate(old_particles(new_size))
+
+            old_particle_ids = this%particle_ids(1:new_size)
+            old_particles = this%particles(1:new_size)
+
+            deallocate(this%particle_ids)
+            deallocate(this%particles)
+            allocate(this%particle_ids(new_size))
+            allocate(this%particles(new_size))
+
+            this%particle_ids(1:new_size) = old_particle_ids
+            this%particles(1:new_size) = old_particles
+
+            deallocate(old_particle_ids)
+            deallocate(old_particles)
+        end if
+    end subroutine resize_particles
 
     subroutine pair_operation(this, compare_func, merge_func)
         class(domain_distribution), intent(inout) :: this
@@ -126,7 +250,9 @@ contains
         chunk_start = this%rank*chunk_size+1
 
         do i=1, this%num_local_particles
-            this%particles(i) = update_func(this%particles(i), chunk_start-1+i)
+            this%particles(i) = update_func( &
+                this%particles(i), this%particle_ids(i) &
+            )
         end do
     end subroutine individual_operation
 
@@ -179,7 +305,7 @@ contains
                 do i=1, this%num_local_particles
                     call print_func( &
                         this%particles(i), &
-                        chunk_start-1+i, &
+                        this%particle_ids(i), &
                         string &
                     )
                     write(*,'(A)') string
@@ -231,6 +357,110 @@ contains
             rank, this%comm, ierror &
         )
     end subroutine get_foreign_list
+
+    subroutine balance_lists(this)
+        class(domain_distribution), intent(inout) :: this
+
+        integer :: old_particle_ids(size(this%particles))
+        type(particle) :: old_particles(size(this%particles))
+
+        integer :: send_sizes(this%nprocs)
+        integer :: recv_sizes(this%nprocs)
+        integer :: send_index_list(this%nprocs, this%num_local_particles)
+        integer, allocatable :: send_particle_ids(:)
+        type(particle), allocatable :: send_particles(:)
+
+        integer :: MPI_particle
+
+        integer :: rank
+        integer :: i
+
+        integer :: ierror
+
+
+        old_particle_ids = this%particle_ids
+        old_particles = this%particles
+
+
+        call generate_MPI_particle(MPI_particle)
+
+        
+        ! Find out what needs to be sent
+        send_sizes = 0
+        send_index_list = -1
+        do i=1, this%num_local_particles
+            rank = get_rank_by_particle(this, this%particles(i))
+            send_sizes(rank+1) = send_sizes(rank+1) + 1
+            send_index_list(rank+1, send_sizes(rank+1)) = i
+        end do
+
+
+        do rank=0, this%nprocs-1
+            ! Find sizes of incoming list
+            call MPI_Gather( &
+                send_sizes(rank+1), 1, MPI_INTEGER, &
+                recv_sizes, 1, MPI_INTEGER, &
+                rank, this%comm, &
+                ierror &
+            )
+
+
+            if(rank .EQ. this%rank) then
+                call resize_particles(this, sum(send_sizes))
+                this%num_local_particles = sum(send_sizes)
+            end if
+
+
+            if(allocated(send_particle_ids)) then
+                deallocate(send_particle_ids, send_particles)
+            end if
+            allocate(send_particle_ids(send_sizes(rank+1)))
+            allocate(send_particles(send_sizes(rank+1)))
+
+
+            send_particle_ids(1:send_sizes(rank+1)) = old_particle_ids( &
+                send_index_list(rank+1,1:send_sizes(rank+1)) &
+            )
+            send_particles(1:send_sizes(rank+1)) = old_particles( &
+                send_index_list(rank+1,1:send_sizes(rank+1)) &
+            )
+
+
+            call MPI_Gatherv( &
+                send_particle_ids, send_sizes(rank+1), MPI_INTEGER, &
+                this%particle_ids, recv_sizes, num_to_off(recv_sizes), &
+                MPI_INTEGER, &
+                rank, this%comm, &
+                ierror &
+            )
+
+
+            call MPI_Gatherv( &
+                send_particles, send_sizes(rank+1), MPI_particle, &
+                this%particles, recv_sizes, num_to_off(recv_sizes), &
+                MPI_particle, &
+                rank, this%comm, &
+                ierror &
+            )
+
+        end do
+
+    contains
+        PURE function num_to_off(sizes)
+            integer, intent(in) :: sizes(:)
+            integer :: num_to_off(size(sizes))
+
+            integer :: i
+            integer :: offset
+
+            offset=0
+            num_to_off = 0
+            do i=1, size(sizes)
+                num_to_off(i) = offset
+                offset = offset+sizes(i)
+            end do
+        end function num_to_off
+    end subroutine balance_lists
 
 
 end module domain_distribution_type
