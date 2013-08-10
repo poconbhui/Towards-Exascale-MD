465a055b3b6e32ba152f95115a8de88f81b6b239
Padraig O Conbhui <poconbhui@gmail.com>
2013-08-10 18:41:26 +0100

    Added some information to test/list_of_tests.rb

--- test/list_of_tests.rb
+++ test/list_of_tests.rb
@@ -1,4 +1,13 @@
-run "particle_type.test", :cores => 4
+# test/list_of_tests.rb
+#
+# Run all the tests here as part of the test suite.
+#
+# This is expected to be run by scripts/run_tests.rb
+#
+# The run command can be found in scripts/mpiexec.rb
+#
+
+run "particle_type.test", :mpi_procs => 4
 
 run "grav_force.test"
 run "integration.test"
