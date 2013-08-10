7ad9ff44d0935e99fce2bf397aca86ab95810112
Pádraig Ó Conbhuí <poconbhui@gmail.com>
2013-08-10 17:10:01 +0100

    Fixed forgotten changes in benchmarking scripts.

--- bench/bench.rb
+++ bench/bench.rb
@@ -1,9 +1,9 @@
-def bench(distribution_name, num_particles, num_reps, num_cores)
+def bench(distribution_name, num_particles, num_reps, mpi_procs, openmp_per_mpi)
     distribution_name = distribution_name.to_s
     num_particles = num_particles.to_s
     num_reps = num_reps.to_s
     num_cores = num_cores.to_s
 
     run "bench #{distribution_name} #{num_particles} #{num_reps}",
-      :cores => num_cores
+      :mpi_procs => mpi_procs, :openmp_per_mpi => openmp_per_mpi
 end
